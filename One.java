17ff5a472ce7ba843e909ee314255e425c2ab533
sohel-git <mrsohel90080@gmail.com>
2026-08-18 15:01:58 +0530

    sohel-modified - for sohel56

--- One.java
+++ One.java
@@ -2,3 +2,13 @@ Line-1
 365-days
 i have worked
 end-sohel
+start
+public class Math {
+    static int add (int i , int j){
+        int output = i+j;
+        System.out.println(" output"+output);
+        return output;
+
+    }
+}
+
